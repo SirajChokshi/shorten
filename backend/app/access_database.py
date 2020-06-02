42a425c31e156f053b4f0f5d460150974f60a047
Siraj <sirajchokshi@gmail.com>
2020-06-02 15:15:32 -0400

    feat: add error codes to database methods

--- backend/app/access_database.py
+++ backend/app/access_database.py
@@ -2,11 +2,9 @@ import sqlite3
 import re
 import gen_words
 
-
 DATABASE = 'links.db'
-code = "Siraj"
-url = "https://sirajchokshi.com"
 
+# returns True if the received url <string> is a valid URL, else returns False
 def is_url_valid(url):
     REGEX_URL = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
@@ -17,6 +15,7 @@ def is_url_valid(url):
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     return (re.match(REGEX_URL, url) is not None)
 
+# returns True if the received code <string> exists in a row in the links table, else False
 def is_code_used(code):
     links_db = sqlite3.connect(DATABASE)
     cur = links_db.cursor()
@@ -24,12 +23,40 @@ def is_code_used(code):
     out = cur.fetchone()
     links_db.commit()
     links_db.close()
-    return bool(out[0])
+    try:
+        return bool(out[0])
+    except:
+        return -1
 
-def add_entry(url, code = gen_words.gen_words()):
-    if is_url_valid(url) and not is_code_used(code):
-        links_db = sqlite3.connect(DATABASE)
-        cur = links_db.cursor()
-        cur.execute("INSERT INTO links VALUES ('{}','{}')".format(code.lower(), url))
-        links_db.commit()
-        links_db.close()
+# adds received url <string> and code <string> as a row in the links table
+# return 0 on success, returns -1 if URL is invalid, returns -2 if code exists in db
+def add_entry(url, code = -1):
+    if code == -1:
+        code = gen_words.gen_words()
+        while not is_code_used(code):
+            code = gen_words.gen_words()
+    if is_url_valid(url):
+        if not is_code_used(code):
+            links_db = sqlite3.connect(DATABASE)
+            cur = links_db.cursor()
+            cur.execute("INSERT INTO links VALUES ('{}','{}')".format(code.lower(), url))
+            links_db.commit()
+            links_db.close()
+            return 0
+        else:
+            return -2
+    else:
+        return -1
+
+# returns entry for the received code, returns -1 if code is not present in database
+def get_entry(code):
+    links_db = sqlite3.connect(DATABASE)
+    cur = links_db.cursor()
+    cur.execute("SELECT * FROM links WHERE code='{}'".format(code.lower()))
+    out = cur.fetchone()
+    links_db.commit()
+    links_db.close()
+    try:
+        return out[1]
+    except:
+        return -1
