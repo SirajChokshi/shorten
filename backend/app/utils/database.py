a8e4e6c60763a10a80c76da4ebec3acb16ccf728
Siraj <sirajchokshi@gmail.com>
2020-06-02 13:14:20 -0400

    chore: disable database.py for data

--- backend/app/utils/database.py
+++ backend/app/utils/database.py
@@ -1,31 +1,33 @@
 import process_words
 import sqlite3
 
-# print(process_words.test())
+# DISABLED FOR DATA SAFETY
 
-conn = sqlite3.connect('../words.db')
+# # print(process_words.test())
 
-c = conn.cursor()
+# conn = sqlite3.connect('../words.db')
 
-words = {
-    'nouns': process_words.genList('../static/index.noun'),
-    'adverbs': process_words.genList('../static/index.adv'),
-    'verbs': process_words.genList('../static/index.verb'),
-}
+# c = conn.cursor()
 
-for tag in words:
+# words = {
+#     'nouns': process_words.genList('../static/index.noun'),
+#     'adverbs': process_words.genList('../static/index.adv'),
+#     'verbs': process_words.genList('../static/index.verb'),
+# }
 
-    # Create table
-    c.execute('''CREATE TABLE {}
-             (word text)'''.format(tag))
+# for tag in words:
 
-    # Insert rows of data
-    for word in words[tag]:
-        c.execute("INSERT INTO {} VALUES ('{}')".format(tag, word))
+#     # Create table
+#     c.execute('''CREATE TABLE {}
+#              (word text)'''.format(tag))
 
-# Save (commit) the changes
-conn.commit()
+#     # Insert rows of data
+#     for word in words[tag]:
+#         c.execute("INSERT INTO {} VALUES ('{}')".format(tag, word))
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+# # Save (commit) the changes
+# conn.commit()
+
+# # We can also close the connection if we are done with it.
+# # Just be sure any changes have been committed or they will be lost.
+# conn.close()
