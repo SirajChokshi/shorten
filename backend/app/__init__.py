d7646ac2f33c56e28b6867fa6597633a947ca1d3
a-ys <40076917+a-ys@users.noreply.github.com>
2020-06-02 16:30:53 -0400

    docs: add comments to __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,17 @@
+# 3rd party imports
 from flask import Flask, redirect
+# local imports
 from . import access_database
 
 app = Flask(__name__)
 
+# homepage
 @app.route("/")
 def hello():
     return "Hello, World!"
 
+# redirects to the page that is linked to the code. if the code has not been 
+# paired, redirects to the notfound page.
 @app.route("/<code>")
 def redirect_from_code(code):
     destination = access_database.get_entry(code)
@@ -15,6 +20,7 @@ def redirect_from_code(code):
     else:
         return redirect("/notfound")
 
+# notfound page
 @app.route("/notfound")
 def not_found():
     return "This shortened link has not yet been created."
