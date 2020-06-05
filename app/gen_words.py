01f07f2c5ae5955d9f463878b35a911bef3fe937
Siraj <sirajchokshi@gmail.com>
2020-06-05 12:23:00 -0400

    docs: add comments for gen_words.py and access_database.py

--- app/gen_words.py
+++ app/gen_words.py
@@ -6,6 +6,7 @@ ADVERBS = 'adverbs'
 VERBS = 'verbs'
 LIST_SIZE = 1000
 
+# returns a hyphenated phrase <string> from three received indexes <int>
 def get_words(noun_index, adverb_index, verb_index):
     db = sqlite3.connect('./app/words.db')
     c = db.cursor()
@@ -24,12 +25,14 @@ def get_words(noun_index, adverb_index, verb_index):
     db.close()
     return phrase[:-1]
 
+# returns three random indexes relative to the specified LIST_SIZE <int> constant
 def gen_index():
     noun_index = random.randrange(0, LIST_SIZE) + 1
     adverb_index = random.randrange(0, LIST_SIZE) + 1
     verb_index = random.randrange(0, LIST_SIZE) + 1
     return noun_index, adverb_index, verb_index
 
+# returns a random hyphenated phrase consisting of a noun, adverb, and verb
 def gen_words():
     n, a, v = gen_index()
     return get_words(n,a,v)
