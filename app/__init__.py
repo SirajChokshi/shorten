06dc57664c3420a6484c2efbc193f65c6d7b3b63
Siraj <sirajchokshi@gmail.com>
2020-06-05 11:03:35 -0400

    feat: styled and status codes for 404 errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # 3rd party imports
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, abort
 # local imports
 from . import access_database
 
@@ -8,9 +8,9 @@ app = Flask(__name__)
 # homepage
 @app.route("/")
 def home():
-    return render_template('index.html', result = -3)
+    return render_template('index.html', result = -3, code = "")
 
-# privacy policy
+# terms of use
 @app.route("/terms")
 def terms():
     return render_template('terms.html')
@@ -25,18 +25,22 @@ def handle_data():
         output = access_database.add_entry(url)
     else:
         output = access_database.add_entry(url, code)
-    return render_template('index.html', result = output, url = url)
+    return render_template('index.html', result = output, url = url, code = "")
 
 # redirects to the page that is linked to the code. if the code has not been 
-# paired, redirects to the 404 page.
+# paired, aborts with a HTTP status 404.
 @app.route("/<code>")
 def redirect_from_code(code):
     destination = access_database.get_entry(code)
     if destination != -1:
         return redirect(destination)
     else:
-        return redirect("/404")
+        abort(404)
 
-@app.route("/404")
-def not_found():
-    return "This shortened link has not yet been created."
+# handle 404 status codes by rendering the styled page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# register the error handler with flask
+app.register_error_handler(404, page_not_found)
